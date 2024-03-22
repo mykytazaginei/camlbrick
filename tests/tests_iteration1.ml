4af7a6274b7c326cfbd08b27fccd9f66f05b6145
ChiiYoYo <chiiyoyo@gmail.com>
2024-03-22 10:37:36 +0100

    Update tests_iteration1.ml

--- tests/tests_iteration1.ml
+++ tests/tests_iteration1.ml
@@ -3,6 +3,7 @@
 #load "CPtestfonc.cma";;
 open CPtestfonc;;
 
+
 (* Initialisation du rapport de test. *)
 test_reset_report();;
 
@@ -12,12 +13,15 @@ let make_vec2(x,y : int * int) : t_vec2 =
   {dx = x ; dy = y}
 ;;
 
-let test_make_vec2 () : unit =
-  let res : t_vec2 = test_exec(make_vec2, "test avec coordonés 3, 5" ,(3,5)) in
-  assert (res = (3,5));;
+(** Test d une valeur positive. *)
+let test_fonc_make_vec2 () : unit =
+  let res : t_vec2 t_test_result =
+     test_exec(make_vec2, "test avec positive values" ,{3;5})
+  assert_equals_result_m("Test dx,dy", {dx = 3; dy = 5}, res)
+;;
 
 (* Appels des fonctions de test *)
-
+test_fonc_make_vec2 ();
 
   
 (* Affiche le rapport de test *)
