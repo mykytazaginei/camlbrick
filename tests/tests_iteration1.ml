8d31f765a9bde6b7d6a360bcd06a1470108505ab
ChiiYoYo <chiiyoyo@gmail.com>
2024-03-23 10:22:36 +0100

    Update tests_iteration1.ml

--- tests/tests_iteration1.ml
+++ tests/tests_iteration1.ml
@@ -33,7 +33,7 @@ let test_fonc_vec2_add () : unit =
 
   (**test d'addition scalaire au vecteur
       @autor Hau NGUYEN*)
-  let test_fonc_vec2_add_scalar () : unit =
+let test_fonc_vec2_add_scalar () : unit =
     let vec = { dx = 1; dy = 2 } in
     let scalar_x = 3 in
     let scalar_y = 4 in
@@ -52,9 +52,9 @@ let test_fonc_vec2_add () : unit =
     assert_equals_result_m ("Test résultat de la multiplication", { dx = 8; dy = 15 }, res)
     ;;
 
-    (**test de multiplication scalaire au vecteur
+(**test de multiplication scalaire au vecteur
       @autor Hau NGUYEN*)
-    let test_fonc_vec2_mult_scalar () : unit =
+let test_fonc_vec2_mult_scalar () : unit =
       let vec = { dx = 2; dy = 3 } in
       let scalar_x = 4 in
       let scalar_y = 5 in
@@ -62,6 +62,20 @@ let test_fonc_vec2_add () : unit =
         test_exec (vec2_mult_scalar, "test de multiplication scalaire au vecteur", (vec, scalar_x, scalar_y)) in
       assert_equals_result_m ("Test résultat de la multiplication scalaire", { dx = 8; dy = 15 }, res);;
 
+(**test de récupération des paramètres du jeu
+          @autor Hau NGUYEN*)
+let test_fonc_param_get () : unit =
+  let params = make_camlbrick_param () in
+  let game = { params = params; ball = {position = make_vec2(0,0); velocity = make_vec2(0,0); size = BS_MEDIUM}; paddle = {
+    position = make_vec2(0, 0);
+    size = PS_SMALL;
+    width = 0;
+    height = 0;
+  }; bricks = [|[||]|]; score = 0; state = GAMEOVER } in
+    let expected_result = params in
+    let res : t_camlbrick_param t_test_result =
+          test_exec (param_get, "test de récupération des paramètres du jeu", game) in
+        assert_equals_result_m ("Test résultat de récupération des paramètres", expected_result, res);;      
 
 
 (* Appels des fonctions de test *)
@@ -70,6 +84,7 @@ test_fonc_vec2_add ();
 test_fonc_vec2_add_scalar ();
 test_fonc_vec2_mult ();
 test_fonc_vec2_mult_scalar ();
+test_fonc_param_get ()
   
 (* Affiche le rapport de test *)
   test_report();;  
