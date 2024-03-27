33ad50e4f1610cf444cb5f9c1d27747bc1e7bbd3
Mykyta Zaginei <nikitazaginey1@gmail.com>
2024-03-27 15:48:15 +0100

    fix t_ball

--- camlbrick.ml
+++ camlbrick.ml
@@ -195,7 +195,7 @@ let vec2_mult_scalar(a,x,y : t_vec2 * int * int) : t_vec2 =
   @autor ZAGINEI Mykyta
   *)
   type t_ball = {
-    position : {dx : int ref; dy : int ref};
+    position : t_vec2 ref;
     velocity : int;
     size : t_ball_size;
   }
