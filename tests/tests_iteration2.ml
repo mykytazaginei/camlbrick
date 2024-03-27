c2c63cc538c87f66609d3014c854282691d9988c
ChiiYoYo <chiiyoyo@gmail.com>
2024-03-27 14:16:17 +0100

    Update tests_iteration2.ml

---
+++ tests/tests_iteration2.ml
@@ -0,0 +1,21 @@
+#use "CPtest.ml";; 
+#load "CPtestfonc.cma";;
+open CPtestfonc;;
+#use "camlbrick.ml";;
+
+
+
+
+
+
+
+(* Appels des fonctions de test *)
+
+
+
+
+
+
+
+(* Affiche le rapport de test *)
+  test_report();;  
